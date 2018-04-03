8b13df27477249535e4edce74768420dc689a3a1
dulitha60 <dulitha60@gmail.com>
2018-04-03 09:33:10 +0800

    Done with Logout system

--- GroceryWatchApp/app/src/main/java/com/example/user/grocerywatchapp/HomeActivity.java
+++ GroceryWatchApp/app/src/main/java/com/example/user/grocerywatchapp/HomeActivity.java
@@ -1,19 +1,55 @@
 package com.example.user.grocerywatchapp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
-    TextView textView;
+
+    ImageButton logout;
+    AlertDialog.Builder builder;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        textView = (TextView)findViewById(R.id.welcome_txt);
-        String message = getIntent().getStringExtra("message");
-        textView.setText(message);
+        logout = (ImageButton) findViewById(R.id.logout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                builder = new AlertDialog.Builder(HomeActivity.this);
+                builder.setTitle("Log out!");
+                builder.setMessage("Are you sure?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        startActivity(new Intent(HomeActivity.this,LoginActivity.class));
+
+                    }
+                }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
+
+
+
+
+
     }
 }
